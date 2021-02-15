0fed3d2d756043e5c6c8916467d069862e3cef77
Solafa khaled <46757940+solafakhaled@users.noreply.github.com>
2021-02-15 20:47:44 +0300

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 import Cases from './Components/Cases'
 import Search from './Components/Search'
 import {useState,useEffect} from 'react'
@@ -6,20 +7,26 @@ import {useState,useEffect} from 'react'
 //it rule is calling other components 
 const App = ()=>{
 
-  //todo: see what you meant by setCase
+  //todo: Take set Case from AddCase Page
+  //todo: Create the Delete Functions and tie it with the server (for Admin only)
   const[cases,setCase]=useState([])
 //function to fetch cases from the database using json, 
 //it awaits for a promise using async
 //the function currently uses a console log to show actions, must tie it with AddPage database
 useEffect(()=>{
-  const fetchCase=async () =>{
-    const res = await fetch('http://localhost:3004/cases')
-    const data=await res.json()
-    console.log(data)
-  }
-fetchCase()
+ const getCases=async()=>{
+   const ServerCases = await fetchCase()
+   setCase(ServerCases)
+ }
+ getCases()
 },[])
-
+//Fetch the tasks from the database
+const fetchCase=async () =>{
+  const res = await fetch('http://localhost:3004/cases')
+  const data=await res.json()
+  console.log(data)
+  return data
+}
   //function for taking input from the search form
   const SubmitSearch=(acase)=>{
     //the function currently displays only on the console,
@@ -39,8 +46,9 @@ fetchCase()
      <Header/>
      <Search onSubmit={SubmitSearch}/>
      {cases.length>0? (<Cases cases={cases} onClick={GotoCase}/>):('There Are No Cases Yet')}
+     <Footer/>
     </div>
-
+     
   )
 }
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+const Footer=()=> {
+    return (
+        <Footer>
+            <p>Copyright &copy; 2021</p>
+            <a href="/about">AboutUs</a>
+        </Footer>
+    )
+}
+
+export default Footer
