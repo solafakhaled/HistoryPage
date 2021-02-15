d04308c74dedf6e6e43638ec23214d65e452cec8
Solafa khaled <46757940+solafakhaled@users.noreply.github.com>
2021-02-15 19:53:56 +0300

    console.log the json data on localhost 3004 (changed from the terminal)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import Header from './Components/Header'
 import Cases from './Components/Cases'
 import Search from './Components/Search'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 //App is the main calling component
-//it is responsable of calling other components 
+//it rule is calling other components 
 const App = ()=>{
+
+  //todo: see what you meant by setCase
   const[cases,setCase]=useState([])
+//function to fetch cases from the database using json, 
+//it awaits for a promise using async
+//the function currently uses a console log to show actions, must tie it with AddPage database
+useEffect(()=>{
+  const fetchCase=async () =>{
+    const res = await fetch('http://localhost:3004/cases')
+    const data=await res.json()
+    console.log(data)
+  }
+fetchCase()
+},[])
+
   //function for taking input from the search form
   const SubmitSearch=(acase)=>{
     //the function currently displays only on the console,
@@ -13,11 +27,12 @@ const App = ()=>{
         console.log(acase)
 
   }
-  //fucntion to navigate to the case page with the givin id
+  //function to navigate to the case page with the givin id
   const GotoCase= (id)=>{
     //this change later to tie with specific case 
     console.log('Go to the case :',id);
   }
+
   //the calling for containers in the main page 
   return (
     <div className="container">
